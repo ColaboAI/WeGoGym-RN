02193a6c08690e96073abcce403c733541406241
sgeunhi <alsrb582@snu.ac.kr>
2022-12-29 17:12:30 +0900

    add login logic

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,27 @@
 
 import React from 'react';
 import MainNavigator from './navigators/Main';
+import Auth from './navigators/Auth';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function App() {
-  return <MainNavigator />;
+  const Stack = createNativeStackNavigator();
+  const isLoggedIn = false;
+  return (
+    <Stack.Navigator
+      initialRouteName="Auth"
+      screenOptions={() => ({ headerShown: false })}>
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen name="MainNavigator" component={MainNavigator} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Auth" component={Auth} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
 }
 
 export default App;
