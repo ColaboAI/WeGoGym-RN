f9d36087ec0f7d8674ac3c30b07636e633bb0b66
GoGiants1 <chooi221@snu.ac.kr>
2023-01-04 11:29:05 +0900

    fix: Hide Tab bar in chat room

--- src/navigators/Chat.tsx
+++ src/navigators/Chat.tsx
@@ -1,11 +1,23 @@
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
+import { useLayoutEffect } from 'react';
 import { ChatListScreen, ChatRoomScreen } from 'screens';
 import CustomNavBarHeader from './CustomNavBarHeader';
-import { ChatStackParamList } from './types';
+import { ChatStackParamList, CustomTabScreenProps } from './types';
 const Stack = createNativeStackNavigator<ChatStackParamList>();
+type Props = CustomTabScreenProps<'Chat'>;
 
-function Chat() {
+function Chat({ navigation, route }: Props) {
+  useLayoutEffect(() => {
+    const routeName = getFocusedRouteNameFromRoute(route);
+    if (routeName === 'ChatRoom') {
+      navigation.setOptions({ tabBarStyle: { display: 'none' } });
+    } else {
+      navigation.setOptions({ tabBarStyle: { display: 'flex' } });
+    }
+  }, [navigation, route]);
+  // TODO: 채팅방 이름 헤더에 띄우기
   const headerTitle = '채팅';
   return (
     <Stack.Navigator
