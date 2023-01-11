4723c42bffd64ecab34c78978b8599a3adc0fc3b
sgeunhi <alsrb582@snu.ac.kr>
2023-01-11 17:50:43 +0900

    add file path

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,33 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@image': './src/image',
+          '@navigators': './src/navigators',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@store': './src/store',
+          '@api': './src/api',
+        },
+      },
+    ],
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
