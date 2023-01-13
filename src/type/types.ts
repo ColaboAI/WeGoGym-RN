c34fd7656d89fa84e915cb08bb4a05b251a3f556
sgeunhi <alsrb582@snu.ac.kr>
2023-01-13 16:38:38 +0900

    add api

--- src/type/types.ts
+++ src/type/types.ts
@@ -1,1 +1,18 @@
-export type WorkoutGoal = { id: number; goal: string; select: boolean };
+type WorkoutGoal = { id: number; goal: string; select: boolean };
+
+type UserBase = {
+  phone_number: string | null;
+};
+
+type UserCreate = UserBase & {
+  username: string | null;
+  gender: string | null;
+  age: string | null;
+  height: string | null;
+  weight: string | null;
+  workout_per_week: string | null;
+  workout_level: string | null;
+  workout_goal: string | null;
+};
+
+export type { WorkoutGoal, UserBase, UserCreate };
