7f533d8bf22c3672fd0b9f4cfd51ee885d9a19e3
sgeunhi <alsrb582@snu.ac.kr>
2023-01-13 16:35:54 +0900

    add folder path

--- babel.config.js
+++ babel.config.js
@@ -24,6 +24,7 @@ module.exports = {
           '@screens': './src/screens',
           '@utils': './src/utils',
           '@store': './src/store',
+          '@type': './src/type',
           '@api': './src/api',
         },
       },
