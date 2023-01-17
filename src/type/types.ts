4d15bea2deececd1e0346b523c7c265041bd96a4
sgeunhi <alsrb582@snu.ac.kr>
2023-01-17 15:02:58 +0900

    Fix User type

--- src/type/types.ts
+++ src/type/types.ts
@@ -11,6 +11,8 @@ type UserCreate = UserBase & {
   height: string | null;
   weight: string | null;
   workout_per_week: string | null;
+  workout_time: string | null;
+  workout_time_how_long: string | null;
   workout_level: string | null;
   workout_goal: string | null;
 };
