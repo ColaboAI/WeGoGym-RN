0e19046e91da037bf6f239dc0510c1fb7eb395da
sgeunhi <alsrb582@snu.ac.kr>
2023-01-31 13:02:54 +0900

    Fix HomeStackScreen Headers

---
+++ src/navigators/NavBarHeader/DetailsNavBarHeader.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Appbar } from 'react-native-paper';
+import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+
+const DetailsNavBarHeader = ({ navigation, back }: NativeStackHeaderProps) => {
+  return (
+    <>
+      <Appbar.Header>
+        {back ? (
+          <Appbar.BackAction
+            onPress={() => {
+              navigation.goBack();
+            }}
+          />
+        ) : null}
+        <Appbar.Content title="" />
+        <Appbar.Action icon="ellipsis-vertical-outline" onPress={() => {}} />
+      </Appbar.Header>
+    </>
+  );
+};
+
+export default DetailsNavBarHeader;
