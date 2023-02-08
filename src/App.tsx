f80364439e18ef722e16839232e1192c9e6ff2ca
GoGiants1 <chooi221@snu.ac.kr>
2023-02-09 02:03:54 +0900

    fix: Fix wrong comparison operator

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,13 @@
  * @format
  */
 
-import React, { createContext, useEffect, useReducer } from 'react';
+import React, { useEffect } from 'react';
 import Auth from './navigators/Auth';
 import MainNavigator from './navigators/Main';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from 'navigators/types';
 const Stack = createNativeStackNavigator<RootStackParamList>();
-import * as SecureStore from 'expo-secure-store';
 import { SplashScreen } from './screens';
-import AuthProvider from './hooks/context/AuthProvider';
 import { useAuthValue, useAuthActions } from './hooks/context/useAuth';
 
 function App() {
@@ -24,36 +22,27 @@ function App() {
   const authActions = useAuthActions();
   useEffect(() => {
     const bootstrapAsync = async () => {
-      let userToken = null;
-      try {
-        userToken = await SecureStore.getItemAsync('userToken');
-      } catch (e) {
-        console.log(e);
-      } finally {
-        authActions.signIn(userToken);
-      }
+      authActions.signIn(authState.userToken);
     };
     bootstrapAsync();
-  }, []);
+  }, [authActions, authState.userToken]);
 
   if (authState.isLoading) {
     return <SplashScreen />;
   }
 
   return (
-    <AuthProvider>
-      <Stack.Navigator screenOptions={() => ({ headerShown: false })}>
-        {authState.userToken == null ? (
-          <>
-            <Stack.Screen name="Auth" component={Auth} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="MainNavigator" component={MainNavigator} />
-          </>
-        )}
-      </Stack.Navigator>
-    </AuthProvider>
+    <Stack.Navigator screenOptions={() => ({ headerShown: false })}>
+      {authState.userToken === null ? (
+        <>
+          <Stack.Screen name="Auth" component={Auth} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="MainNavigator" component={MainNavigator} />
+        </>
+      )}
+    </Stack.Navigator>
   );
 }
 
