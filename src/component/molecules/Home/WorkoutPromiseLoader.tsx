8828e69bfe9f290b77626f422c9c2f855ed22aba
sgeunhi <alsrb582@snu.ac.kr>
2023-02-08 17:17:38 +0900

    Add Workout Promise Loader

---
+++ src/component/molecules/Home/WorkoutPromiseLoader.tsx
@@ -0,0 +1,40 @@
+import { useTheme } from '@react-navigation/native';
+import React from 'react';
+import ContentLoader, {
+  IContentLoaderProps,
+  Rect,
+} from 'react-content-loader/native';
+import { View } from 'react-native';
+
+const WorkoutPromiseLoader = (
+  props: JSX.IntrinsicAttributes & IContentLoaderProps,
+) => {
+  const theme = useTheme();
+  return (
+    <View>
+      <ContentLoader
+        speed={2}
+        width={400}
+        height={800}
+        viewBox="0 0 400 800"
+        backgroundColor={theme.colors.background}
+        foregroundColor={theme.colors.card}
+        {...props}>
+        <Rect x="17" y="0" rx="5" ry="5" width="35%" height="25" />
+        <Rect x="17" y="35" rx="5" ry="5" width="88%" height="25" />
+        <Rect x="17" y="70" rx="5" ry="5" width="83%" height="23" />
+        <Rect x="17" y="105" rx="5" ry="5" width="35%" height="25" />
+        <Rect x="17" y="140" rx="5" ry="5" width="88%" height="25" />
+        <Rect x="17" y="175" rx="5" ry="5" width="86%" height="23" />
+        <Rect x="17" y="210" rx="5" ry="5" width="35%" height="25" />
+        <Rect x="17" y="245" rx="5" ry="5" width="88%" height="25" />
+        <Rect x="17" y="280" rx="5" ry="5" width="86%" height="23" />
+        <Rect x="17" y="315" rx="5" ry="5" width="35%" height="25" />
+        <Rect x="17" y="350" rx="5" ry="5" width="88%" height="25" />
+        <Rect x="17" y="385" rx="5" ry="5" width="86%" height="23" />
+      </ContentLoader>
+    </View>
+  );
+};
+
+export default WorkoutPromiseLoader;
