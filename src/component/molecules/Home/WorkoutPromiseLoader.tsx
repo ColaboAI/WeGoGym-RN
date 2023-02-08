f0dc8653017d2fe468e3150abc26398ea7b5c9f9
sgeunhi <alsrb582@snu.ac.kr>
2023-02-08 17:43:42 +0900

    Add WorkoutPromiseLoader

--- src/component/molecules/Home/WorkoutPromiseLoader.tsx
+++ src/component/molecules/Home/WorkoutPromiseLoader.tsx
@@ -15,23 +15,14 @@ const WorkoutPromiseLoader = (
       <ContentLoader
         speed={2}
         width={400}
-        height={800}
-        viewBox="0 0 400 800"
+        height={1000}
+        viewBox="0 0 400 1000"
         backgroundColor={theme.colors.background}
         foregroundColor={theme.colors.card}
         {...props}>
-        <Rect x="17" y="0" rx="5" ry="5" width="35%" height="25" />
-        <Rect x="17" y="35" rx="5" ry="5" width="88%" height="25" />
-        <Rect x="17" y="70" rx="5" ry="5" width="83%" height="23" />
-        <Rect x="17" y="105" rx="5" ry="5" width="35%" height="25" />
-        <Rect x="17" y="140" rx="5" ry="5" width="88%" height="25" />
-        <Rect x="17" y="175" rx="5" ry="5" width="86%" height="23" />
-        <Rect x="17" y="210" rx="5" ry="5" width="35%" height="25" />
-        <Rect x="17" y="245" rx="5" ry="5" width="88%" height="25" />
-        <Rect x="17" y="280" rx="5" ry="5" width="86%" height="23" />
-        <Rect x="17" y="315" rx="5" ry="5" width="35%" height="25" />
-        <Rect x="17" y="350" rx="5" ry="5" width="88%" height="25" />
-        <Rect x="17" y="385" rx="5" ry="5" width="86%" height="23" />
+        <Rect x="15" y="0" rx="5" ry="5" width="350" height="150" />
+        <Rect x="15" y="160" rx="5" ry="5" width="350" height="150" />
+        <Rect x="15" y="320" rx="5" ry="5" width="350" height="150" />
       </ContentLoader>
     </View>
   );
