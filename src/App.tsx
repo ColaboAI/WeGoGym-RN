d8163b6f4bb2bbca268af647f976533d99f7a717
GoGiants1 <chooi221@snu.ac.kr>
2023-02-09 15:43:28 +0900

    chore: remove template comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, { useEffect } from 'react';
 import Auth from './navigators/Auth';
 import MainNavigator from './navigators/Main';
