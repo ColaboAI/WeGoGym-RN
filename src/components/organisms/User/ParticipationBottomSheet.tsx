f495c5ba0cb7e1efca842420fcd2052c91fcf412
sgeunhi <alsrb582@snu.ac.kr>
2023-02-28 01:31:06 +0900

    feat: add participation bottomSheet

---
+++ src/components/organisms/User/ParticipationBottomSheet.tsx
@@ -0,0 +1,143 @@
+import { Keyboard, Platform, StyleSheet, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetTextInput,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
+import { Button, IconButton, Text, useTheme } from 'react-native-paper';
+
+import { BottomSheetDefaultBackdropProps } from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types';
+
+type Props = {
+  isBottomSheetOpen: boolean;
+  setIsBottomSheetOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const ParticipationBottomSheet = (props: Props) => {
+  const theme = useTheme();
+
+  const bottomSheetRef = React.useRef<BottomSheet>(null);
+  const [requestText, setRequestText] = useState<string>('');
+  //   const [gymData, setGymData] = useState<Gym[] | null>(null);
+  //   const [filteredGymData, setFilteredGymData] = useState<Gym[] | null>(null);
+
+  const iosSnapPoints = React.useMemo(() => ['50%'], []);
+  const androidSnapPoints = React.useMemo(() => ['90%'], []);
+
+  const renderBackdrop = useCallback(
+    (
+      backdropProps: JSX.IntrinsicAttributes & BottomSheetDefaultBackdropProps,
+    ) => (
+      <BottomSheetBackdrop
+        {...backdropProps}
+        pressBehavior="close"
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+        opacity={0.5}
+      />
+    ),
+    [],
+  );
+
+  useEffect(() => {
+    if (props.isBottomSheetOpen) {
+      bottomSheetRef.current?.expand();
+    } else {
+      bottomSheetRef.current?.close();
+    }
+
+    return () => {
+      setRequestText('');
+    };
+  }, [props.isBottomSheetOpen]);
+
+  return (
+    <BottomSheet
+      ref={bottomSheetRef}
+      index={-1}
+      snapPoints={Platform.OS === 'ios' ? iosSnapPoints : androidSnapPoints}
+      backdropComponent={renderBackdrop}
+      enablePanDownToClose={true}
+      onClose={() => {
+        props.setIsBottomSheetOpen(false);
+      }}
+      android_keyboardInputMode={'adjustResize'}>
+      <BottomSheetView style={{ flex: 1 }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginHorizontal: 12,
+            marginVertical: 12,
+          }}>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: 'bold',
+              lineHeight: 24,
+            }}>
+            운동 파트너에게 보낼 메세지를{'\n'}작성해주세요
+          </Text>
+          <IconButton
+            icon="close"
+            size={24}
+            onPress={() => {
+              props.setIsBottomSheetOpen(false);
+            }}
+          />
+        </View>
+        <View style={{ marginBottom: 12 }}>
+          <BottomSheetTextInput
+            value={requestText}
+            placeholder="ex) 같이 운동해요!"
+            onChangeText={value => setRequestText(value)}
+            returnKeyType="done"
+            maxLength={50}
+            multiline={true}
+            style={[
+              styles.textInput,
+              {
+                backgroundColor: theme.colors.secondaryContainer,
+                color: theme.colors.onBackground,
+              },
+            ]}
+          />
+        </View>
+      </BottomSheetView>
+      <Button
+        mode="contained"
+        style={{ margin: 12, borderRadius: 10 }}
+        onPress={() => {
+          props.setIsBottomSheetOpen(false);
+          Keyboard.dismiss();
+        }}>
+        참여하기
+      </Button>
+    </BottomSheet>
+  );
+};
+
+export default ParticipationBottomSheet;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    paddingHorizontal: 12,
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    marginHorizontal: 12,
+    marginBottom: 12,
+    padding: 12,
+    height: 100,
+    borderRadius: 12,
+    color: 'gray',
+    fontSize: 16,
+    fontWeight: '500',
+    lineHeight: 24,
+    textAlign: 'left',
+  },
+});
