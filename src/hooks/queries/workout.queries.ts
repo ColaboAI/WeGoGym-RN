5e0d1447d6cd9d401bddf01b3835260cef0bc345
sgeunhi <alsrb582@snu.ac.kr>
2023-02-27 14:45:11 +0900

    chore: add initialData

--- src/hooks/queries/workout.queries.ts
+++ src/hooks/queries/workout.queries.ts
@@ -22,6 +22,39 @@ export function useGetWorkoutQuery(limit: number, offset: number) {
     queryKey: ['getWorkout', limit, offset],
     queryFn: () => getWorkoutPromise({ limit, offset }),
     retry: 1,
+    initialData: [
+      {
+        id: 1,
+        title: '운동 약속 제목',
+        description: '운동 약속 설명',
+        maxParticipants: 5,
+        promiseTime: '2021-08-01T00:00:00.000Z',
+        recruitEndTime: '2021-08-01T00:00:00.000Z',
+        isPrivate: false,
+        gymInfo: {
+          id: 1,
+          status: 'ACTIVE',
+          name: '헬스장 이름',
+          address: '헬스장 주소',
+          zipCode: '헬스장 우편번호',
+        },
+        participants: [
+          {
+            id: 1,
+            user: {
+              id: 1,
+              username: '유저 이름',
+              profile: {
+                uri: '유저 프로필 이미지',
+                size: 100,
+              },
+            },
+          },
+        ],
+        createdAt: '2021-08-01T00:00:00.000Z',
+        updatedAt: '2021-08-01T00:00:00.000Z',
+      },
+    ],
     onError: (error: Error) => {
       Alert.alert(`운동 약속을 가져오는데 실패하였습니다: ${error.message}`);
       console.log(error);
