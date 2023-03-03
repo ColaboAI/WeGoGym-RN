973d2f613274bd04ca9af84af464f952c5502042
GoGiants1 <chooi221@snu.ac.kr>
2023-03-03 14:04:31 +0900

    feat: Add Chat Message queries

--- src/api/api.ts
+++ src/api/api.ts
@@ -326,6 +326,24 @@ async function getMyChatList(param: {
   }
 }
 
+async function getChatRoom(id: string): Promise<ChatRoom> {
+  try {
+    const res = await apiClient.get(`/chat/rooms/${id}`);
+    return res.data;
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function getChatMessages(id: string): Promise<ChatMessage[]> {
+  try {
+    const res = await apiClient.get(`/chat/rooms/${id}/messages`);
+    return res.data;
+  } catch (e) {
+    throw e;
+  }
+}
+
 export {
   postLogin,
   postRegister,
@@ -345,4 +363,6 @@ export {
   getRecommendedMates,
   getUserInfo,
   getMyChatList,
+  getChatRoom,
+  getChatMessages,
 };
