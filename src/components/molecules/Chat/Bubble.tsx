3bd2a81bf88137266206987d3e55879bc9cb8d7f
GoGiants1 <chooi221@snu.ac.kr>
2023-03-08 13:47:02 +0900

    feat: Done getting Chat Room message from back

--- src/components/molecules/Chat/Bubble.tsx
+++ src/components/molecules/Chat/Bubble.tsx
@@ -1,38 +1,66 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
-import { Text, useTheme } from 'react-native-paper';
-
-const Bubble = ({ _id, text, createdAt, user, isLeft }: Message) => {
+import { Surface, Text, useTheme } from 'react-native-paper';
+import { getValueFor } from '/store/secureStore';
+import { getRelativeTime } from '/utils/util';
+import CustomAvatar from '/components/atoms/Common/CustomAvatar';
+type Props = {
+  getUserInfo: (id: string) => User | undefined;
+} & Message;
+const Bubble = ({ text, createdAt, userId, getUserInfo }: Props) => {
   const theme = useTheme();
 
+  const myId = getValueFor('userId');
+
+  const isLeft = myId !== userId;
+  const userInfo = getUserInfo(userId);
+
   return (
-    <View
-      style={[
-        isLeft ? styles.leftBubble : styles.rightBubble,
-        {
-          backgroundColor: isLeft
-            ? theme.colors.secondary
-            : theme.colors.primary,
-        },
-      ]}>
-      <Text
-        style={{
-          color: isLeft ? theme.colors.onSecondary : theme.colors.onPrimary,
-        }}>
-        {user.name}
-      </Text>
-      <Text
-        style={{
-          color: isLeft ? theme.colors.onSecondary : theme.colors.onPrimary,
-        }}>
-        {text}
-      </Text>
-      <Text
-        style={{
-          color: isLeft ? theme.colors.onSecondary : theme.colors.onPrimary,
-        }}>
-        {createdAt.toString()}
-      </Text>
+    <View style={styles.bubbleContainer}>
+      {isLeft && (
+        <View style={styles.avatarContainer}>
+          <CustomAvatar
+            username={userInfo?.username ?? '알 수 없음'}
+            profilePic={userInfo?.profilePic}
+            size={30}
+            style={styles.avatar}
+          />
+          <Text
+            ellipsizeMode="tail"
+            numberOfLines={1}
+            style={[
+              {
+                color: theme.colors.onBackground,
+              },
+              styles.userName,
+            ]}>
+            {userInfo?.username ?? '알 수 없음'}
+          </Text>
+        </View>
+      )}
+      <Surface
+        style={[
+          isLeft ? styles.leftBubble : styles.rightBubble,
+          {
+            backgroundColor: isLeft
+              ? theme.colors.secondary
+              : theme.colors.primary,
+          },
+        ]}
+        elevation={3}>
+        <Text
+          style={{
+            color: isLeft ? theme.colors.onSecondary : theme.colors.onPrimary,
+          }}>
+          {text}
+        </Text>
+        <Text
+          style={{
+            color: isLeft ? theme.colors.onSecondary : theme.colors.onPrimary,
+          }}>
+          {getRelativeTime(createdAt)}
+        </Text>
+      </Surface>
     </View>
   );
 };
@@ -40,14 +68,19 @@ const Bubble = ({ _id, text, createdAt, user, isLeft }: Message) => {
 export default Bubble;
 
 const styles = StyleSheet.create({
+  bubbleContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginBottom: 5,
+  },
   leftBubble: {
     alignSelf: 'flex-start',
     marginLeft: 10,
     marginRight: 60,
     padding: 15,
-    borderTopLeftRadius: 20,
+    borderTopLeftRadius: 0,
     borderTopRightRadius: 20,
-    borderBottomLeftRadius: 0,
+    borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
     marginBottom: 10,
   },
@@ -58,9 +91,25 @@ const styles = StyleSheet.create({
     marginRight: 10,
     padding: 15,
     borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
+    borderTopRightRadius: 0,
     borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 0,
+    borderBottomRightRadius: 20,
     marginBottom: 10,
+  },
+
+  avatarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+    marginLeft: '1%',
+  },
+
+  userName: {
+    textAlignVertical: 'center',
+    fontWeight: '600',
+    marginLeft: '1%',
+  },
+  avatar: {
+    alignSelf: 'center',
   },
 });
--- src/hooks/queries/chat.queries.ts
+++ src/hooks/queries/chat.queries.ts
@@ -1,7 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
-import { getChatMessages, getChatRoom, getMyChatList } from '@api/api';
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+import {
+  getChatMessages,
+  getChatRoom,
+  getDirectChatRoom,
+  getMyChatList,
+  postChatRoom,
+} from '@api/api';
 import { Alert } from 'react-native';
 import { AxiosError } from 'axios';
+import { getValueFor } from '/store/secureStore';
 
 export function useMyChatListQuery(limit: number, offset: number) {
   return useQuery({
@@ -37,17 +49,77 @@ export function useChatRoomQuery(chatRoomId: string) {
   });
 }
 
-export function useChatMessagesQuery(chatRoomId: string) {
-  return useQuery({
-    queryKey: ['chatMessages', chatRoomId],
-    queryFn: () => getChatMessages(chatRoomId),
+export function useChatRoomMessagesQuery(
+  chatRoomId: string | undefined,
+  limit: number,
+  offset: number,
+) {
+  return useInfiniteQuery({
+    queryKey: ['chatMessages', chatRoomId, limit, offset],
+
+    queryFn: props =>
+      getChatMessages(chatRoomId, limit, props.pageParam || offset),
     retry: 1,
     onError: (error: AxiosError) => {
-      Alert.alert(`채팅 메시지를 가져오는데 실패하였습니다: ${error.message}`);
+      Alert.alert(
+        `채팅 메시지를 가져오는데 실패하였습니다: ${error.code}, ${error.message}`,
+      );
     },
     onSuccess(data) {
       console.log('채팅 메시지를 가져왔습니다: ', data);
     },
     suspense: true,
+    getNextPageParam: (lastPage, _) => {
+      if (lastPage.nextCursor === undefined) {
+        return undefined;
+      }
+      return lastPage.nextCursor;
+    },
+    getPreviousPageParam: (firstPage, _) => {
+      if (firstPage.nextCursor === undefined) {
+        return undefined;
+      }
+      return firstPage.nextCursor;
+    },
+    enabled: !!chatRoomId,
+  });
+}
+
+export function useDirectChatRoomQuery(userId: string) {
+  return useQuery({
+    queryKey: ['directChatRoom', userId],
+    queryFn: () => getDirectChatRoom(userId),
+    retry: 0,
+    onError: (error: AxiosError) => {
+      Alert.alert(`채팅방 정보를 가져오는데 실패하였습니다: ${error.message}`);
+      if (error.response?.status === 404) {
+        console.log('채팅방이 없습니다.');
+      }
+    },
+    onSuccess(data) {
+      console.log('채팅방 정보를 가져왔습니다: ', data);
+    },
+    suspense: true,
+    enabled: !!userId,
+  });
+}
+
+export function useChatRoomMutation() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: postChatRoom,
+    onError: (error: AxiosError) => {
+      Alert.alert(`채팅방 생성에 실패하였습니다: ${error.message}`);
+    },
+    onSuccess(data) {
+      console.log('채팅방을 생성했습니다: ', data);
+      if (data.isGroupChat) {
+        queryClient.setQueryData(['chatRoom', data.id], data);
+      } else {
+        const myId = getValueFor('userId');
+        const recieverId = data.members.find(m => m.user.id !== myId)?.id;
+        queryClient.setQueryData(['directChatRoom', recieverId], data);
+      }
+    },
   });
 }
