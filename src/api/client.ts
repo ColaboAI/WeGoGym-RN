a9d6766f161b3088ab7cd6b06c66f0eeeb6ec2e3
GoGiants1 <chooi221@snu.ac.kr>
2023-03-14 00:56:37 +0900

    chore: reuse base url

--- src/api/client.ts
+++ src/api/client.ts
@@ -5,7 +5,7 @@ import applyCaseMiddleware from 'axios-case-converter';
 import { useCallback, useMemo, useState } from 'react';
 import { Alert, Platform } from 'react-native';
 import { refreshAccessToken } from './api';
-const BASE_URL =
+export const BASE_URL =
   Platform.OS === 'ios'
     ? 'http://127.0.0.1:8000/api/v1'
     : 'http://10.0.2.2:8000/api/v1';
