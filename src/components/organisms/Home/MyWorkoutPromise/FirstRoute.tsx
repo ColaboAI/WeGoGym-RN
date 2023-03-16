b48352ff3e53a813b8e0abe2ee5dba651707bbcd
sgeunhi <alsrb582@snu.ac.kr>
2023-03-16 16:05:06 +0900

    feat: add my workout promise route component

---
+++ src/components/organisms/Home/MyWorkoutPromise/FirstRoute.tsx
@@ -0,0 +1,110 @@
+import React, { Suspense, useCallback } from 'react';
+import { useQueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import { StyleSheet, View } from 'react-native';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { Button, Text, useTheme } from 'react-native-paper';
+import WorkoutPromiseCard from '/components/molecules/Home/WorkoutPromiseCard';
+import WorkoutPromiseLoader from '/components/molecules/Home/WorkoutPromiseLoader';
+import { useGetWorkoutWrittenByUserIdQuery } from '/hooks/queries/workout.queries';
+
+type Props = {
+  limit: number;
+  offset: number;
+  navigateToPromiseDetails: (id: string) => void;
+};
+
+// 내가 만든 운동 약속
+const FirstRoute = ({ limit, offset, navigateToPromiseDetails }: Props) => {
+  const { reset } = useQueryErrorResetBoundary();
+  const theme = useTheme();
+  const workoutPromiseWrittenByMeQuery = useGetWorkoutWrittenByUserIdQuery(
+    'me',
+    limit,
+    offset,
+  );
+
+  const renderError = useCallback(
+    (resetErrorBoundary: () => void) => (
+      <View style={styles.errorContainer}>
+        <Text>운동을 불러올 수 없습니다!</Text>
+        <Button onPress={() => resetErrorBoundary()}>다시 시도</Button>
+      </View>
+    ),
+    [],
+  );
+
+  return (
+    <Suspense fallback={<WorkoutPromiseLoader />}>
+      <ErrorBoundary
+        onReset={reset}
+        fallbackRender={({ resetErrorBoundary }) =>
+          renderError(resetErrorBoundary)
+        }>
+        <View
+          style={[
+            styles.container,
+            { backgroundColor: theme.colors.background },
+          ]}>
+          {workoutPromiseWrittenByMeQuery.data ? (
+            <FlatList
+              data={workoutPromiseWrittenByMeQuery.data.items}
+              keyExtractor={item => item.id}
+              contentContainerStyle={styles.workoutPromiseContainer}
+              initialNumToRender={5}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  key={`workout-promise-container-${item.id}`}
+                  onPress={() => {
+                    navigateToPromiseDetails(item.id);
+                  }}>
+                  <WorkoutPromiseCard
+                    key={`workout-promise-${item.id}`}
+                    {...item}
+                  />
+                </TouchableOpacity>
+              )}
+              showsVerticalScrollIndicator={false}
+              ListEmptyComponent={
+                <View style={styles.emptyContainer}>
+                  <Text style={styles.emptyText}>운동 약속이 없습니다🙃</Text>
+                  <Text style={styles.emptyText}>
+                    새로운 운동 약속을 만들어보세요!
+                  </Text>
+                </View>
+              }
+            />
+          ) : (
+            <WorkoutPromiseLoader />
+          )}
+        </View>
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  errorContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  workoutPromiseContainer: {
+    flexGrow: 1,
+  },
+  emptyContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emptyText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
+
+export default FirstRoute;
