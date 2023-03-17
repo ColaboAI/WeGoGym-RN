7131512776fa23d81cd79c12b78294bff18816f0
sgeunhi <alsrb582@snu.ac.kr>
2023-03-17 11:21:42 +0900

    feat: fix loader

--- src/components/molecules/Home/NotificationCard.tsx
+++ src/components/molecules/Home/NotificationCard.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Card, Button, Text } from 'react-native-paper';
 
-const NotificationCard = () => {
+type NotificationProps = {
+  _id: string;
+  title: string;
+  message: string;
+  onPressApprove: (id: string) => void;
+  onPressReject: (id: string) => void;
+  createdAt: string;
+};
+
+// 운동 약속 참여자 승인 및 거절
+const NotificationCard = ({
+  _id,
+  title,
+  message,
+  onPressApprove,
+  onPressReject,
+}: NotificationProps) => {
   return (
     <Card>
-      <Card.Title title="Card Title" subtitle="Card Subtitle" />
+      <Card.Title title={title} />
       <Card.Content>
-        <Text>Card content</Text>
+        <Text>{message}</Text>
       </Card.Content>
-      <Card.Cover source={{ uri: 'https://picsum.photos/200/300' }} />
       <Card.Actions>
-        <Button>Cancel</Button>
-        <Button>Ok</Button>
+        <Button
+          mode="contained"
+          onPress={() => {
+            onPressApprove(_id);
+          }}>
+          승인
+        </Button>
+        <Button
+          mode="contained-tonal"
+          onPress={() => {
+            onPressReject(_id);
+          }}>
+          거절
+        </Button>
       </Card.Actions>
     </Card>
   );
