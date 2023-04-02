556cfb7ec65f7faf0f47b422e48565bac6b915ba
sgeunhi <alsrb582@snu.ac.kr>
2023-04-02 19:07:35 +0900

    feat: add navigator to noti-workout

--- src/components/molecules/Home/NotificationCard.tsx
+++ src/components/molecules/Home/NotificationCard.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Card, Button, Text, useTheme } from 'react-native-paper';
+import { Pressable, StyleSheet, View } from 'react-native';
+import { Card, Text, useTheme } from 'react-native-paper';
 import CustomAvatar from '/components/atoms/Common/CustomAvatar';
 import {
   usePutWorkoutParticipantAcceptMutation,
@@ -19,6 +18,7 @@ type NotificationProps = {
   recipient: WorkoutParticipantsRead;
   createdAt: Date;
   navigateToUserDetails: (id: string) => void;
+  navigateToWorkoutDetails: (id: string) => void;
 };
 
 // 운동 약속 참여자 승인 및 거절
@@ -31,6 +31,7 @@ const NotificationCard = ({
   recipient,
   createdAt,
   navigateToUserDetails,
+  navigateToWorkoutDetails,
 }: NotificationProps) => {
   const theme = useTheme();
   const body = getNotificationBody(notificationType);
@@ -70,47 +71,76 @@ const NotificationCard = ({
   ]);
 
   return (
-    <View key={`notification-${id}`} style={styles.notificationCardContainer}>
-      <Card>
-        <Card.Content>
-          <>
-            <View style={styles.infoContainer}>
-              <View style={styles.avatarContainer}>
-                <TouchableOpacity
-                  onPress={() => {
-                    navigateToUserDetails(sender.id);
-                  }}>
-                  <CustomAvatar
-                    size={30}
-                    profilePic={sender.user.profilePic}
-                    username={sender.user.username}
-                  />
-                </TouchableOpacity>
-              </View>
-              <View style={styles.messsageContainer}>
-                <Text variant="titleSmall">
-                  <Text style={styles.username}>{sender.user.username}</Text>님
-                  {body} {message}{' '}
-                  <Text
-                    variant="titleSmall"
-                    style={{ color: theme.colors.onPrimaryContainer }}>
-                    {getRelativeTime(createdAt)}
+    <Pressable
+      onPress={() => navigateToWorkoutDetails(recipient.workoutPromiseId)}>
+      <View key={`notification-${id}`} style={styles.notificationCardContainer}>
+        <Card>
+          <Card.Content>
+            <>
+              <View style={styles.infoContainer}>
+                <View style={styles.avatarContainer}>
+                  <Pressable
+                    onPress={() => {
+                      navigateToUserDetails(sender.userId);
+                    }}>
+                    <CustomAvatar
+                      size={30}
+                      profilePic={sender.user.profilePic}
+                      username={sender.user.username}
+                    />
+                  </Pressable>
+                </View>
+                <View style={styles.messsageContainer}>
+                  <Text variant="titleSmall">
+                    <Text style={styles.username}>{sender.user.username}</Text>
+                    님{body} {message}{' '}
+                    <Text
+                      variant="titleSmall"
+                      style={{ color: theme.colors.onPrimaryContainer }}>
+                      {getRelativeTime(createdAt)}
+                    </Text>
                   </Text>
+                </View>
+              </View>
+            </>
+          </Card.Content>
+          <Card.Actions>
+            <Pressable onPress={onPressAccept}>
+              <View
+                style={[
+                  styles.buttonBox,
+                  { backgroundColor: theme.colors.primary },
+                ]}>
+                <Text
+                  variant="bodyLarge"
+                  style={[
+                    styles.buttonText,
+                    { color: theme.colors.onPrimary },
+                  ]}>
+                  승인
                 </Text>
               </View>
-            </View>
-          </>
-        </Card.Content>
-        <Card.Actions>
-          <Button mode="contained" onPress={onPressAccept}>
-            승인
-          </Button>
-          <Button mode="contained-tonal" onPress={onPressReject}>
-            거절
-          </Button>
-        </Card.Actions>
-      </Card>
-    </View>
+            </Pressable>
+            <Pressable onPress={onPressReject}>
+              <View
+                style={[
+                  styles.buttonBox,
+                  { backgroundColor: theme.colors.background },
+                ]}>
+                <Text
+                  variant="bodyLarge"
+                  style={[
+                    styles.buttonText,
+                    { color: theme.colors.onBackground },
+                  ]}>
+                  거절
+                </Text>
+              </View>
+            </Pressable>
+          </Card.Actions>
+        </Card>
+      </View>
+    </Pressable>
   );
 };
 
@@ -135,6 +165,14 @@ const styles = StyleSheet.create({
   bodyContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  buttonBox: {
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontWeight: '500',
   },
 });
 
