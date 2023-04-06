453ae0dc05b7a731166b89eaf984ea56f0303f6d
sgeunhi <alsrb582@snu.ac.kr>
2023-04-06 16:33:16 +0900

    style: add divider below card

--- src/components/molecules/Home/WorkoutPromiseCard.tsx
+++ src/components/molecules/Home/WorkoutPromiseCard.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
-import { Text, Card, useTheme } from 'react-native-paper';
+import { Text, Card, useTheme, Divider } from 'react-native-paper';
 import {
   getLocaleDate,
   getLocaleTime,
@@ -32,73 +32,81 @@ const WorkoutPromiseCard = ({
   return (
     <View key={`workout-promise-card-${id}`} style={style.promiseCardContainer}>
       {adminUserInfo ? (
-        <Card elevation={1}>
-          <Card.Title
-            title={title}
-            left={props => (
-              <CustomAvatar
-                {...props}
-                size={30}
-                profilePic={adminUserInfo?.profilePic}
-                username={adminUserInfo?.username ?? '알 수 없음'}
-              />
-            )}
-            right={props => (
-              <Text {...props} variant="labelSmall">
-                {adminUserInfo?.username ?? '알 수 없음'}님
-              </Text>
-            )}
-            leftStyle={style.leftBox}
-            rightStyle={style.rightBox}
-          />
-          <Card.Content>
-            <>
-              <View style={style.infoBox}>
-                <Icon
-                  name="calendar-outline"
-                  size={18}
-                  color={theme.colors.onBackground}
-                  style={style.icon}
+        <>
+          <Card
+            mode="contained"
+            style={{
+              borderRadius: 0,
+              backgroundColor: theme.colors.background,
+            }}>
+            <Card.Title
+              title={title}
+              left={props => (
+                <CustomAvatar
+                  {...props}
+                  size={30}
+                  profilePic={adminUserInfo?.profilePic}
+                  username={adminUserInfo?.username ?? '알 수 없음'}
                 />
-                <Text>
-                  {getLocaleDate(promiseTime)} {getLocaleTime(promiseTime)}
+              )}
+              right={props => (
+                <Text {...props} variant="labelSmall">
+                  {adminUserInfo?.username ?? '알 수 없음'}님
                 </Text>
-              </View>
-              <View style={style.infoBox}>
-                <Icon
-                  name="location-outline"
-                  size={18}
-                  color={theme.colors.onBackground}
-                  style={style.icon}
-                />
-                <Text>{gymInfo ? gymInfo.name : '위치 미정'}</Text>
-              </View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}>
+              )}
+              leftStyle={style.leftBox}
+              rightStyle={style.rightBox}
+            />
+            <Card.Content>
+              <>
                 <View style={style.infoBox}>
                   <Icon
-                    name="people-outline"
+                    name="calendar-outline"
                     size={18}
                     color={theme.colors.onBackground}
                     style={style.icon}
                   />
                   <Text>
-                    {isAcceptedParticipant(participants).length}/
-                    {maxParticipants} 참여 중
+                    {getLocaleDate(promiseTime)} {getLocaleTime(promiseTime)}
                   </Text>
                 </View>
-                <Text
-                  variant="labelSmall"
-                  style={{ color: theme.colors.onBackground }}>
-                  {getRelativeTime(updatedAt)}
-                </Text>
-              </View>
-            </>
-          </Card.Content>
-        </Card>
+                <View style={style.infoBox}>
+                  <Icon
+                    name="location-outline"
+                    size={18}
+                    color={theme.colors.onBackground}
+                    style={style.icon}
+                  />
+                  <Text>{gymInfo ? gymInfo.name : '위치 미정'}</Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <View style={style.infoBox}>
+                    <Icon
+                      name="people-outline"
+                      size={18}
+                      color={theme.colors.onBackground}
+                      style={style.icon}
+                    />
+                    <Text>
+                      {isAcceptedParticipant(participants).length}/
+                      {maxParticipants} 참여 중
+                    </Text>
+                  </View>
+                  <Text
+                    variant="labelSmall"
+                    style={{ color: theme.colors.onBackground }}>
+                    {getRelativeTime(updatedAt)}
+                  </Text>
+                </View>
+              </>
+            </Card.Content>
+          </Card>
+          <Divider />
+        </>
       ) : (
         <WorkoutPromiseLoader />
       )}
@@ -108,7 +116,7 @@ const WorkoutPromiseCard = ({
 
 const style = StyleSheet.create({
   promiseCardContainer: {
-    padding: 12,
+    padding: 6,
   },
   leftBox: {
     marginRight: 0,
