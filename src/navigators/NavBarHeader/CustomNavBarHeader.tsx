9b4fbcde778977c2a36c00d794ea01db44923300
sgeunhi <alsrb582@snu.ac.kr>
2023-04-06 14:47:03 +0900

    style: delete menu component

--- src/navigators/NavBarHeader/CustomNavBarHeader.tsx
+++ src/navigators/NavBarHeader/CustomNavBarHeader.tsx
@@ -1,6 +1,6 @@
 // import { StyleSheet } from 'react-native';
 import React, { useLayoutEffect, useMemo } from 'react';
-import { Appbar, Menu, useTheme } from 'react-native-paper';
+import { Appbar, useTheme } from 'react-native-paper';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 import { Route } from '@react-navigation/native';
 import { ChatParamList } from '../types';
@@ -10,10 +10,7 @@ type Props = NativeStackHeaderProps & {
 
 const CustomNavBarHeader = ({ navigation, back, route }: Props) => {
   const theme = useTheme();
-  const [visible, setVisible] = React.useState(false);
   const [newBack, setNewBack] = React.useState(back);
-  const openMenu = () => setVisible(true);
-  const closeMenu = () => setVisible(false);
   const headerTitle = useMemo(() => {
     const routeName = route.name;
     switch (routeName) {
@@ -51,41 +48,8 @@ const CustomNavBarHeader = ({ navigation, back, route }: Props) => {
         <Appbar.BackAction onPress={() => navigation.goBack()} />
       ) : null}
       <Appbar.Content title={headerTitle} color={theme.colors.onBackground} />
-      {!newBack ? (
-        <Menu
-          visible={visible}
-          onDismiss={closeMenu}
-          anchor={
-            <Appbar.Action
-              icon="menu"
-              color={theme.colors.onBackground}
-              onPress={openMenu}
-            />
-          }>
-          <Menu.Item
-            onPress={() => {
-              console.log('Option 1 was pressed');
-            }}
-            title="Option 1"
-          />
-          <Menu.Item
-            onPress={() => {
-              console.log('Option 2 was pressed');
-            }}
-            title="Option 2"
-          />
-          <Menu.Item
-            onPress={() => {
-              console.log('Option 3 was pressed');
-            }}
-            title="Option 3"
-          />
-        </Menu>
-      ) : null}
     </Appbar.Header>
   );
 };
 
 export default CustomNavBarHeader;
-
-// const styles = StyleSheet.create({});
