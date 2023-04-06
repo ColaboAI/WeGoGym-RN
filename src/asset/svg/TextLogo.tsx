31f78da7d1dd97544c580579a64af8966cacff22
sgeunhi <alsrb582@snu.ac.kr>
2023-04-06 14:45:21 +0900

    style: chagne logo size

--- src/asset/svg/TextLogo.tsx
+++ src/asset/svg/TextLogo.tsx
@@ -5,8 +5,8 @@ import { WithLocalSvg } from 'react-native-svg';
 export default function TextLogo() {
   const theme = useTheme();
   if (theme.dark) {
-    return <WithLocalSvg asset={require('./darkLogo.svg')} width={100} />;
+    return <WithLocalSvg asset={require('./darkLogo.svg')} width={120} />;
   }
 
-  return <WithLocalSvg asset={require('./lightLogo.svg')} width={100} />;
+  return <WithLocalSvg asset={require('./lightLogo.svg')} width={120} />;
 }
