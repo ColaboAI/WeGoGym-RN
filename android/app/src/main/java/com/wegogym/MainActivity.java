33267c35497137645f2933934bd1b7dacf98612a
sgeunhi <alsrb582@snu.ac.kr>
2023-04-11 20:10:25 +0900

    feat: apply android splash

--- android/app/src/main/java/com/wegogym/MainActivity.java
+++ android/app/src/main/java/com/wegogym/MainActivity.java
@@ -5,6 +5,8 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -40,6 +42,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    RNBootSplash.init(this);
+    super.onCreate(savedInstanceState);
   }
 }
