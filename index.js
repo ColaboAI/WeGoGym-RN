9eef52b47d1640d4180cd0a73e29d39b378223be
GoGiants1 <chooi221@snu.ac.kr>
2023-05-16 02:23:45 +0900

    fix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { name as appName } from './app.json';
 import Main from './src/Main';
 import { onMessageInBackground } from '/utils/notification';
 import messaging from '@react-native-firebase/messaging';
+import notifee, { EventType } from '@notifee/react-native';
 
 Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
@@ -21,24 +22,24 @@ function HeadlessCheck({ isHeadless }) {
 }
 
 messaging().setBackgroundMessageHandler(onMessageInBackground);
-// notifee.onBackgroundEvent(async ({ type, detail }) => {
-//   const { notification, pressAction } = detail;
-//   if (
-//     notification === undefined ||
-//     pressAction === undefined ||
-//     type === undefined ||
-//     notification.id === undefined
-//   ) {
-//     return;
-//   }
-//   // Check if the user pressed the "Mark as read" action
-//   if (type === EventType.ACTION_PRESS && pressAction.id === 'mark-as-read') {
-//     // Decrement the count by 1
-//     await notifee.decrementBadgeCount();
+notifee.onBackgroundEvent(async ({ type, detail }) => {
+  const { notification, pressAction } = detail;
+  if (
+    notification === undefined ||
+    pressAction === undefined ||
+    type === undefined ||
+    notification.id === undefined
+  ) {
+    return;
+  }
+  // Check if the user pressed the "Mark as read" action
+  if (type === EventType.ACTION_PRESS && pressAction.id === 'mark-as-read') {
+    // Decrement the count by 1
+    await notifee.decrementBadgeCount();
 
-//     // Remove the notification
-//     await notifee.cancelNotification(notification.id);
-//   }
-// });
+    // Remove the notification
+    await notifee.cancelNotification(notification.id);
+  }
+});
 
 AppRegistry.registerComponent(appName, () => HeadlessCheck);
