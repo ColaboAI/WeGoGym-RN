8b5bfc586d314f2203d0bb3cc71d3625be798169
sgeunhi <alsrb582@snu.ac.kr>
2023-08-16 16:26:05 +0900

    feat: modify ios api key dev to prod

--- ios/WeGoGym/AppDelegate.mm
+++ ios/WeGoGym/AppDelegate.mm
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyB1tM37ODNnChN3zprnAQv9KWESBZWw8aw"]; // add this line using the api key obtained from Google Console
+  [GMSServices provideAPIKey:@"AIzaSyAMpmwVrE9dLJD3oZIjqKbbKUQocEG50RM"]; // add this line using the api key obtained from Google Console
   self.moduleName=@"WeGoGym";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
