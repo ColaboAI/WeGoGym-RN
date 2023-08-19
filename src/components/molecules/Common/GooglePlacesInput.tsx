3b21a370eaac9e9cbfec86f95a1ef3aec8e0d3cd
sgeunhi <alsrb582@snu.ac.kr>
2023-08-19 18:28:54 +0900

    feat: customize Google Place Search Bar

--- src/components/molecules/Common/GooglePlacesInput.tsx
+++ src/components/molecules/Common/GooglePlacesInput.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { PLACE_API_KEY } from '@env';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { TextInput } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
 
 type Props = {
   onSelectPlace: (data: any, details: any) => void;
@@ -9,28 +11,45 @@ type Props = {
 const GooglePlacesInput = (props: Props) => {
   return (
     <GooglePlacesAutocomplete
-      minLength={2}
-      placeholder="위치 검색"
+      minLength={1}
+      placeholder="장소 이름이나 주소를 입력해주세요."
       query={{
         key: PLACE_API_KEY,
         language: 'ko',
         components: 'country:kr',
       }}
-      keyboardShouldPersistTaps={'handled'}
       fetchDetails={true}
       onPress={(data, details) => {
         props.onSelectPlace(data, details);
-        console.log('data', data);
-        console.log('details', details);
       }}
       onFail={error => console.log(error)}
       onNotFound={() => console.log('no results')}
+      keyboardShouldPersistTaps={'handled'}
       keepResultsAfterBlur={true}
       enablePoweredByContainer={false}
+      nearbyPlacesAPI="GoogleReverseGeocoding"
+      textInputProps={{
+        InputComp: TextInput,
+        mode: 'outlined',
+        left: (
+          <TextInput.Icon
+            icon="search-outline"
+            size={15}
+            style={styles.searchBtnIcon}
+            disabled={true}
+          />
+        ),
+      }}
       currentLocation={true}
       currentLocationLabel="현재 위치"
     />
   );
 };
+
+const styles = StyleSheet.create({
+  searchBtnIcon: {
+    top: 4,
+  },
+});
 
 export default GooglePlacesInput;
