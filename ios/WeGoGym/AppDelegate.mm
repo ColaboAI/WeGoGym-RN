5534b6976d12a760ef05101047c5f799e30b3cb0
GoGiants1 <chooi221@snu.ac.kr>
2023-08-30 15:23:36 +0900

    fix: Hide google API KEY

--- ios/WeGoGym/AppDelegate.mm
+++ ios/WeGoGym/AppDelegate.mm
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyAMpmwVrE9dLJD3oZIjqKbbKUQocEG50RM"]; // add this line using the api key obtained from Google Console
+  [GMSServices provideAPIKey:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"GOOGLE_MAPS_API_KEY"]]; // add this line using the api key obtained from Google Console
   self.moduleName=@"WeGoGym";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
